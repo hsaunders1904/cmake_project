12a5822b91b93be2ad8928d5aabefc094456d5a2
Harry Saunders <hsaunders1904@icloud.com>
2019-08-31 01:20:33 +0100

    Add conanfile.txt and run conan install in CMake
    Remove .cmake files "findCatch" and "findConan" and use Conan's CMake
    generator instead.
    
    Run "conan install" during CMake configure time using cmake as the
    generator. We can then include the conanbuildinfo.cmake file to get
    links to any requirements that were downloaded when running conan
    install.
    
    Add Catch2 to the conanfile.txt requirements and link the dependency to
    our test target.

--- cmake/findCatch.cmake
+++
@@ -1,9 +0,0 @@
-set(CATCH_VERSION "2.6.0")
-mark_as_advanced(CATCH_VERSION)
-
-conan_cmake_run(
-  REQUIRES "Catch2/${CATCH_VERSION}@catchorg/stable"
-  BASIC_SETUP
-  CMAKE_TARGETS
-  BUILD_TYPE "None"
-)
