18bca9139d4e051e5c4d438e5453cd3517194acc
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-02-27 18:20:47 -0500

    Add build and package scripts

---
+++ tools/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function echo_and_run() {
+    echo "+ $1"
+    eval "$1"
+}
+
+readonly root_dir="$(realpath $(dirname $0)/..)"
+readonly build_dir="${root_dir}/build"
+
+echo_and_run "cd ${build_dir}"
+echo_and_run "cmake ${root_dir} -B${build_dir}" && \
+echo_and_run "cmake --build ${build_dir}"
---
+++ tools/package.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function echo_and_run() {
+    echo "+ $1"
+    eval "$1"
+}
+
+readonly root_dir="$(realpath $(dirname $0)/..)"
+readonly build_dir="${root_dir}/build"
+readonly install_dir="${build_dir}/install"
+
+echo_and_run "cd ${build_dir}"
+echo_and_run "cmake ${root_dir} -DCMAKE_INSTALL_PREFIX=${install_dir}"
+echo_and_run "cmake --build ${build_dir} --target install"
