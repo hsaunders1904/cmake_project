8aa1bcb5c17a590e8f072718ac9fa411ef953c79
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-02-27 19:00:49 -0500

    Create test.sh script

---
+++ tools/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function echo_and_run() {
+    echo "+ $1"
+    eval "$1"
+}
+
+readonly root_dir="$(realpath $(dirname $0)/..)"
+readonly build_dir="${root_dir}/build"
+
+echo_and_run "cd build"
+echo_and_run "./bin/cmake_project"
+echo_and_run "ctest --output-on-failure -T Test --no-compress-output"""
